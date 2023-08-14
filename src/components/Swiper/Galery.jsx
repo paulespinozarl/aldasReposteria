c5ef661e4f28b20dcf77ddb10dceab37756092ef
Paulespinozarl <paulespinozarl@gmail.com>
2023-08-13 22:48:47 -0300

    pink2

--- src/components/Swiper/Galery.jsx
+++ src/components/Swiper/Galery.jsx
@@ -39,6 +39,7 @@ export const Galery = () => {
         <SwiperSlide className='flex justify-center'> <SwapComp image={ galleta_1 } texto="Agregar descripción"/> </SwiperSlide>
         <SwiperSlide className='flex justify-center'> <SwapComp image={ torta_6 } texto="Agregar descripción"/> </SwiperSlide>
         <div style={{ color: '#fbcfe8'}} className='swiper-button-next'>
+          <GrFormNext size={30}/>
         </div>
         <GrFormPrevious size={30} className='swiper-button-prev'/>
       </Swiper>
