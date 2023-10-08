6d1e25fc716b8970eb9b39ad9cb5a4d2e443a863
Paulespinozarl <paulespinozarl@gmail.com>
2023-10-08 16:17:04 -0300

    xs

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,26 +12,26 @@ export const Footer = () => {
 
   return (
     <>
-        <footer className="h-32 lg:h-48 bg-gray-400 grid grid-cols-2 grid-rows-4 items-center justify-around">
-            <div className="flex flex-col ml-7 lg:ml-0 lg:flex-row justify-center row-start-1 row-end-6 text-gray-700">
-                <Link to={'/'} className="lg:text-sm lg:mr-4 text-xs hover:underline leading-loose hover:cursor-pointer">
+        <footer className="h-32 lg:h-48 bg-gray-400 grid items-center justify-around">
+            <div className="flex lg:flex-row justify-center text-gray-700 mt-2">
+                <Link to={'/'} className="lg:text-sm mr-2 lg:mr-4 text-xs hover:underline leading-loose hover:cursor-pointer">
                   Inicio
                 </Link>
-                <Link to={'/recetas'} className="lg:text-sm lg:mr-4 text-xs hover:underline leading-loose hover:cursor-pointer">
+                <Link to={'/recetas'} className="lg:text-sm mr-2 lg:mr-4 text-xs hover:underline leading-loose hover:cursor-pointer">
                   Productos
                 </Link>
-                <a href="#" className="lg:text-sm lg:mr-4 text-xs hover:underline leading-loose hover:cursor-pointer">
+                <a href="#" className="lg:text-sm mr-2 lg:mr-4 text-xs hover:underline leading-loose hover:cursor-pointer">
                   Sobre Mi
                 </a>
                 <Link to={'/contacto'} className="lg:text-sm lg:mr-4 text-xs hover:underline leading-loose hover:cursor-pointer">
                   Contáctame
                 </Link>
             </div>
-            <div className="flex items-center justify-around row-start-1 row-end-6 text-gray-700">
-                <a href="https://www.instagram.com/aldasreposteria/" target="_blank"><FaInstagram className="transition-transform duration-200 hover:scale-125" size={30}/></a>
-                <a href="#"><PiYoutubeLogoLight className=" transition-transform duration-200 hover:scale-125" size={35}/></a>
+            <div className="flex items-center justify-around text-gray-700">
+                <a href="https://www.instagram.com/aldasreposteria/" target="_blank"><FaInstagram className="transition-transform duration-200 hover:scale-125" size={25}/></a>
+                <a href="#"><PiYoutubeLogoLight className=" transition-transform duration-200 hover:scale-125" size={30}/></a>
             </div>
-            <div className="grid col-start-1 col-end-4 items-center justify-center mb-1">
+            <div className="grid items-center justify-center mb-1">
                 <span className="text-ssm text-gray-800">Desarrollado por Paul Espinoza - © 2023</span>
                 <div className="flex justify-center">
                   <a href="https://wa.me/+541130085208" target="_blank" rel="noopener noreferrer" className="transition-transform duration-200 hover:scale-125 flex justify-center mr-2">
